470b8e661a4d2a048657f1aafc5f2201596acca3
Hoa Thai Nguyen <thaihoa.nguyen@trustingsocial.com>
2019-06-20 11:54:54 +0700

    Update Snapshots

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ function NotFoundPage() {
   return (
     <Layout>
       <SEO title="404: Not found" />
-      <h1>NOT FOUND</h1>
+      <h1>OOPS</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Layout>
   );
