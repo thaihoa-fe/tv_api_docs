9e14d6c10628e49463047a066bccdd372d3d39af
Hoa Thai Nguyen <thaihoa.nguyen@trustingsocial.com>
2019-06-20 14:50:59 +0700

    Add client side redirect

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,5 +70,6 @@ module.exports = {
         icon: 'src/assets/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-client-side-redirect'
   ]
 };
