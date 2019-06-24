9dc0ec543b5f9eff27d60eaefa428ad7b170a4aa
Hoa Thai Nguyen <thaihoa.nguyen@trustingsocial.com>
2019-06-24 15:58:54 +0700

    Fix tests

--- src/components/TOC/__tests__/TOC.js
+++ src/components/TOC/__tests__/TOC.js
@@ -5,20 +5,29 @@ import { PureTOC as TOC } from '..';
 import categories from './__fixtures__/categories.json';
 
 describe('TOC', () => {
+  const history = {
+    location: {
+      pathname: '/about_trusting_social/',
+    },
+  };
+
   it('renders correctly with empty categories', () => {
-    const tree = renderer.create(<TOC />).toJSON();
+    const tree = renderer.create(<TOC history={history} />).toJSON();
     expect(tree).toBe(null);
   });
 
   it('renders correctly with categories', () => {
-    const testRenderer = renderer.create(<TOC categories={categories} />);
+    const testRenderer = renderer.create(<TOC history={history} categories={categories} />);
     const tree = testRenderer.toJSON();
 
     expect(tree).toMatchSnapshot();
   });
 
   it('renders with correct quantity of MenuItem with categories', () => {
-    const testInstance = renderer.create(<TOC categories={categories} />).root;
-    expect(testInstance.findAllByType(MenuItem)).toHaveLength(categories.length);
+    const matchItems = categories.filter(
+      item => item.path === history.location.pathname || item.depth === 1
+    );
+    const testInstance = renderer.create(<TOC history={history} categories={categories} />).root;
+    expect(testInstance.findAllByType(MenuItem)).toHaveLength(matchItems.length);
   });
 });
